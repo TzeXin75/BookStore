618d73eecf3cd278ff6f005bae74f1ce0b2df02a
Tze <146824048+TzeXin75@users.noreply.github.com>
2025-12-22 00:01:44 +0800

    array user id

--- remove_from_cart.php
+++ remove_from_cart.php
@@ -2,14 +2,14 @@
 session_start();
 require_once 'config/db_connect.php';
 
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user']['user_id'])) {
     header("Location: login.php");
     exit();
 }
 
 // Fixed: Check for both 'id' or 'cart_id' to be safe
 $cart_id = $_GET['id'] ?? $_GET['cart_id'] ?? null;
-$user_id = $_SESSION['user_id'];
+$user_id = $_SESSION['user']['user_id'];
 
 if ($cart_id) {
     try {
