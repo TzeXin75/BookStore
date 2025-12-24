d2ee55520e76d6950c508f24be91338cf754227d
rouyu06 <limry-wm25@student.tarc.edu.my>
2025-12-24 23:08:47 +0800

    .

--- header.php
+++ header.php
@@ -1,17 +1,10 @@
 <?php require '_base.php'; ?>
 
-<header class="site-header">
-
 <head>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
 <body>
-<header>
-    <div id="info"><?= temp('info') ?></div>
-
-
 <header class="site-header">
-
     <div class="header-container">
         <div class="header-left">
             <a href="index.php" class="logo">BookStore</a>
@@ -87,6 +80,7 @@
             </div>
         </div>
     </nav>
+    <div id="info"><?= temp('info') ?></div>
 </header>
 
 <style>
